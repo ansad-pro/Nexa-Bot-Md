3d80e01625051fd4103a6688b14d67b163c9dcd2
arakbox3 <115354476+arakbox3@users.noreply.github.com>
2026-03-26 14:39:53 +0530

    Update loader.js
    Signed-off-by: arakbox3 <115354476+arakbox3@users.noreply.github.com>

--- settings/loader.js
+++ settings/loader.js
@@ -1,5 +1,5 @@
-// © 2026 arun•°Cumar. All Rights Reserved.  
-import { fquoted } from './quoted.js'; 
+// © 2026 arun•°Cumar. All Rights Reserved.   
+import { getQuoted } from './quoted.js'; 
 import ownerHandler from '../plugins/owner.js';
 import menuHandler from '../plugins/menu.js';  
 import aliveHandler from '../plugins/alive.js';  
@@ -10,34 +10,40 @@ import stickerHandler from '../plugins/sticker.js';
 export async function handleCommands(commandName, sock, msg, args, extra) {  
     const { isOwner, isAdmin } = extra;  
   
-    const quoted = fquoted(msg);   
+    //Get quoted 
+    const quoted = getQuoted(msg);   
   
-    if (commandName === 'menu' || commandName === 'help') {  
-          
-        await menuHandler(sock, msg, args, { isOwner, isAdmin, quoted });  
-    }   
+    // commands checking 
+    switch (commandName) {
+        case 'menu':
+        case 'help':
+            await menuHandler(sock, msg, args, { isOwner, isAdmin, quoted });
+            break;
 
-    else if (commandName === 'owner') {  
-        await ownerHandler(sock, msg, args, { isOwner, isAdmin, quoted });  
-    }  
-    
-    else if (commandName === 'alive') {  
-        await aliveHandler(sock, msg, args, { isOwner, isAdmin, quoted });  
-    }  
-      
-    else if (commandName === 'ping') {  
-        await pingHandler(sock, msg, args, { isOwner, isAdmin, quoted });  
-    }  
-       
-    else if (commandName === 'url' || commandName === 'link') {  
-        await urlHandler(sock, msg, args, { isOwner, isAdmin, quoted });  
-    }  
-  
-    else if (commandName === 'sticker' || commandName === 's') {  
-        await stickerHandler(sock, msg, args, { isOwner, isAdmin, quoted });  
-    }  
-      
-    else {  
-        console.log(`Unknown command: ${commandName}`);  
-    }  
-}; 
+        case 'owner':
+            await ownerHandler(sock, msg, args, { isOwner, isAdmin, quoted });
+            break;
+
+        case 'alive':
+            await aliveHandler(sock, msg, args, { isOwner, isAdmin, quoted });
+            break;
+
+        case 'ping':
+            await pingHandler(sock, msg, args, { isOwner, isAdmin, quoted });
+            break;
+
+        case 'url':
+        case 'link':
+            await urlHandler(sock, msg, args, { isOwner, isAdmin, quoted });
+            break;
+
+        case 'sticker':
+        case 's':
+            await stickerHandler(sock, msg, args, { isOwner, isAdmin, quoted });
+            break;
+
+        default:
+            console.log(`Unknown command: ${commandName}`);
+            break;
+    }
+}
