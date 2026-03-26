c6f16d120781f150598b0d3ac383863ddf48d3ce
arakbox3 <115354476+arakbox3@users.noreply.github.com>
2026-03-26 14:37:31 +0530

    Update quoted.js
    Signed-off-by: arakbox3 <115354476+arakbox3@users.noreply.github.com>

--- settings/quoted.js
+++ settings/quoted.js
@@ -1,28 +1,28 @@
-// © 2026 Arun Cumar. All Rights Reserved.
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import chalk from 'chalk'; 
 
-export const fquoted = {
-    key: {
-        fromMe: false,
-        participant: "0@s.whatsapp.net",
-        remoteJid: "status@broadcast" 
-    },
-    message: {
-        newsletterAdminInviteMessage: {
-            newsletterJid: "120363422992896382@newsletter",
-            newsletterName: "⚡Nexa-Bot-MD OFFICIAL",
-            caption: "🚀 NEXA-BOT-MD : THE NEXT GEN BOT",
-            inviteExpiration: Date.now() + 1800000 
+export const getQuoted = (msg) => {
+    return {
+        key: {
+            fromMe: false,
+            participant: "0@s.whatsapp.net",
+            remoteJid: "status@broadcast" 
+        },
+        message: {
+            newsletterAdminInviteMessage: {
+                newsletterJid: "120363422992896382@newsletter",
+                newsletterName: "⚡NEXA-BOT-MD OFFICIAL",
+                caption: "🚀 NEXA-BOT-MD : THE NEXT GEN BOT",
+                inviteExpiration: Date.now() + 1800000,
+                 
+            }
         }
-    }
+    };
 };
 
 const __filename = fileURLToPath(import.meta.url);
-
 fs.watchFile(__filename, () => {
     fs.unwatchFile(__filename);
     console.log(chalk.redBright('─╼[ ') + chalk.white('File Updated: ') + chalk.greenBright(__filename.split('/').pop()) + chalk.redBright(' ]╾─'));
-  
 });
